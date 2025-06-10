12975d7992be15b0fd31a4c292294da6b99f8875
rahmanediting <abdurrahmanediting.id@gmail.com>
2025-06-10 20:09:18 +0700

    Add files via upload

---
+++ webserver.py
@@ -0,0 +1,26 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import time
+
+hostName = "localhost"
+serverPort = 8080 #You can choose any available port; by default, it is 8000
+
+
+class MyServer(BaseHTTPRequestHandler):  
+    def do_GET(self): 
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes("<html><head><title>https://testserver.com</title></head>", "utf-8"))
+        self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
+        self.wfile.write(bytes("<body>", "utf-8"))
+        self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
+        self.wfile.write(bytes("</body></html>", "utf-8"))
+
+if __name__ == "__main__":        
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))  #Server starts
+try:
+    webServer.serve_forever()
+except KeyboardInterrupt:
+    pass
+webServer.server_close()  #Executes when you hit a keyboard interrupt, closing the server
